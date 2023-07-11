4cd7bfe73ba6a71655bd700743fe79088a6d31ab
rchaubey <rchaubey@micron.com>
2023-07-11 18:47:25 +0530

    todo list backend app

---
+++ README.md
@@ -0,0 +1,3 @@
+<h2> To do list app with adding new todo item and deleting todo list item functionality , database : mongodb </h2>
+
+![image](https://github.com/rcramh/Backend_express/assets/131397354/3e48b16d-eb5b-4eb2-8026-c95e9804bab8)
---
+++ app.js
@@ -0,0 +1,77 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+//to use ejs templating
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+//connect with local mongodb database 
+mongoose.connect('mongodb://localhost:27017/todolistDB' , {useNewUrlParser: true} );
+//mongodb+srv://rahulchaubey68rj:rahulluhar@cluster0.vqukqis.mongodb.net/todolistDB
+//mongodb://localhost:27017/todolistDB
+//create schema for todolistDB collection
+const itemsSchema = {
+  name : String
+};
+
+//create a new mongoose model
+const Item = mongoose.model("Item",itemsSchema);
+
+const item1 = new Item({
+  name : "Workout"
+});
+
+//item1.save();
+
+
+app.get("/", async function(req, res) {
+
+const day = date.getDate();
+
+  let ItemList = await Item.find({});
+
+  res.render("list", {listTitle: "Today ", newListItems: ItemList});
+
+});
+
+app.post("/", function(req, res){
+
+  const itemName = req.body.newItem;
+  const newItem = new Item({
+    name :  itemName
+  }); 
+
+  newItem.save();
+
+  res.redirect("/");
+});
+
+app.post("/delete", function(req, res){
+
+  const itemid = req.body.checkbox;
+
+  console.log(itemid);
+
+  Item.deleteMany({_id : itemid}).then(function(){
+  console.log("Todo item deleted");//Success
+  }).catch(function(error){
+  console.log(error);//Failure
+  });
+
+  res.redirect("/");
+});
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
