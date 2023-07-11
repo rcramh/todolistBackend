793498f10846a402f07ef299c8d6dc051b8d9689
rchaubey <rchaubey@micron.com>
2023-07-11 19:36:52 +0530

    testing 2

--- app.js
+++ app.js
@@ -72,6 +72,7 @@ res.send("hello")
 
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Server started on port ${port}');
 });
